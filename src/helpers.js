1dabb13da361579d488ba0badc2e90f8f91508ce
rahmadalfianm <60783259+rahmadalfianm@users.noreply.github.com>
2020-09-25 18:23:42 +0700

    update btn decrement jika qty dibawah 1

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,11 @@
 const incrementQty = (qty) => parseInt(qty) + 1;
-const decrementQty = (qty) => parseInt(qty) - 1;
+const decrementQty = (qty) => {
+    if(parseInt(qty)-1 == 0){
+        return 1;
+    }else{
+        return parseInt(qty) - 1;
+    }
+};
 const subTotal = (price, qty) => price * qty;
 
 module.exports = { 
